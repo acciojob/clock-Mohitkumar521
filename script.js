a5c283f1d6106a58634e5e177788a6577ed504fa
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-31 15:14:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function updateTimer() {
+            const timerElement = document.getElementById('timer');
+            const now = new Date();
+            const hours = now.getHours().toString().padStart(2, '0');
+            const minutes = now.getMinutes().toString().padStart(2, '0');
+            const seconds = now.getSeconds().toString().padStart(2, '0');
+            const date = now.toDateString();
+            const time = `${hours}:${minutes}:${seconds}`;
+            
+            timerElement.textContent = `Date: ${date} | Time: ${time}`;
+        }
+
+        // Update the timer immediately and then every second
+        updateTimer();
+        setInterval(updateTimer, 1000);
